b61233a17459b43b4f1ab8f05e349c3358f00741
Benaya Trabelsi <benaya7@gmail.com>
2025-11-26 15:24:55 +0200

    added t-test and normalized columns to their meaning.

--- scripts/h1b_dataset/estimate_h1b_salaries.py
+++ scripts/h1b_dataset/estimate_h1b_salaries.py
@@ -101,7 +101,7 @@ def main():
     # Save to CSV
     output_path = f'data/h1b-lca-disclosure-data-2020-2024/llm_estimated_salaries{args.model.split("/")[-1]}.csv'
     if is_debug_mode:
-        output_path = 'data/h1b-lca-disclosure-data-2020-2024/llm_estimated_salaries_debug{args.model.split("/")[-1]}.csv'
+        output_path = f'data/h1b-lca-disclosure-data-2020-2024/llm_estimated_salaries_debug{args.model.split("/")[-1]}.csv'
     h1b_df.to_csv(output_path, index=False)
 
     llm.delete_client()
--- scripts/salaries_dataset/estimate_salaries.py
+++ scripts/salaries_dataset/estimate_salaries.py
@@ -127,9 +127,9 @@ def main():
         salaries_df.at[idx, 'estimated_salary_in_usd'] = est
 
     # Save to CSV
-    output_path = 'data/salaries-for-data-science-jobs/llm_estimated_salaries.csv'
+    output_path = f'data/salaries-for-data-science-jobs/llm_estimated_salaries{args.model.split("/")[-1]}.csv'
     if is_debug_mode:
-        output_path = 'data/salaries-for-data-science-jobs/llm_estimated_salaries_debug.csv'
+        output_path = f'data/salaries-for-data-science-jobs/llm_estimated_salaries_debug{args.model.split("/")[-1]}.csv'
     salaries_df.to_csv(output_path, index=False)
 
     llm.delete_client()
