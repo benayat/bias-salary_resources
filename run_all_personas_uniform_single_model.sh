0b03302c4a352155f63232738d239fb4a93ebde0
Benaya Trabelsi <benaya7@gmail.com>
2025-12-01 22:09:59 +0200

    set welch and hc3 and only one persona for now.

--- run_all_personas_uniform_single_model.sh
+++ run_all_personas_uniform_single_model.sh
@@ -24,11 +24,18 @@ uv run scripts/h1b_dataset/sample_dataset.py \
     --ai-soc-weight-mode "$soc_weight"
 
 # Run estimation for all personas in a single command
+#uv run scripts/h1b_dataset/estimate_h1b_salaries.py \
+#    --model "$model" \
+#    --llm-config home_small \
+#    --input-csv-file data/h1b-lca-disclosure-data-2020-2024/h1b_2024_sampled-${soc_weight}.csv \
+#    --personas-to-use ai_minimalistic_no_behavior_mod ai_minimalistic_behavior_mod ai_extended_no_behavior_mod ai_extended_behavior_mod human_minimalistic_no_behavior_mod human_minimalistic_behavior_mod human_extended_no_behavior_mod human_extended_behavior_mod neutral_minimalistic_no_behavior_mod neutral_extended_no_behavior_mod salary_estimator
+
 uv run scripts/h1b_dataset/estimate_h1b_salaries.py \
     --model "$model" \
     --llm-config home_small \
     --input-csv-file data/h1b-lca-disclosure-data-2020-2024/h1b_2024_sampled-${soc_weight}.csv \
-    --personas-to-use ai_minimalistic_no_behavior_mod ai_minimalistic_behavior_mod ai_extended_no_behavior_mod ai_extended_behavior_mod human_minimalistic_no_behavior_mod human_minimalistic_behavior_mod human_extended_no_behavior_mod human_extended_behavior_mod neutral_minimalistic_no_behavior_mod neutral_extended_no_behavior_mod salary_estimator
+    --personas-to-use salary_estimator
+
 
 # Compare with paired t-test
 #uv run scripts/h1b_dataset/compare_with_paired_ttest_verbose.py \
